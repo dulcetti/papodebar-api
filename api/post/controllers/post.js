449c5a0a08deba471a9599505c4443ceda96c5b2
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-05-25 22:42:43 -0300

    feat: add perPage and page consts

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -7,7 +7,6 @@ module.exports = {
       'Authorization':
         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjIxNjIzMjMwLCJleHAiOjE2MjQyMTUyMzB9.nU2b7uRnhhcrcKcD67xXXv-Hh_Fpv0fwijc9ZVHTTvE',
     };
-    const { data } = await axios.get('https://www.papodebar.com/wp-json/wp/v2/posts?per_page=1');
     const usersWp = await axios.get('https://www.papodebar.com/wp-json/wp/v2/users?per_page=100');
     const categoriesWp = await axios.get(
       'https://www.papodebar.com/wp-json/wp/v2/categories?per_page=100'
@@ -51,47 +50,56 @@ module.exports = {
       return categoryOfWpPost.slug;
     };
 
-    const posts = await Promise.all(
-      data.map(
-        (post) =>
-          new Promise(async (resolve, reject) => {
-            const {
-              author,
-              content: { rendered: contentRendered },
-              categories,
-              date,
-              date_gmt,
-              excerpt: { rendered: excerptRendered },
-              featured_media_src_url,
-              id,
-              slug,
-              title: { rendered: titleRendered },
-            } = post;
+    let page = 1;
+    const perPage = 1;
 
-            try {
-              const downloaded = await strapi.config.functions.download(featured_media_src_url);
-              const [{ id: fileId }] = await strapi.config.functions.upload(downloaded);
+    async function test() {
+      const { data } = await axios.get(
+        `https://www.papodebar.com/wp-json/wp/v2/posts?per_page=${perPage}&page=${page}`
+      );
 
-              const postData = {
-                author: verifyUserOfPost(author),
-                content: contentRendered,
-                categories: verifyCategoryOfPost(categories[0]),
-                original_date: date,
-                publish_date: date_gmt,
-                excerpt: excerptRendered,
-                featured_image: [fileId],
-                wp_id: id,
+      /*const posts = await Promise.all(
+        data.map(
+          (post) =>
+            new Promise(async (resolve, reject) => {
+              const {
+                author,
+                content: { rendered: contentRendered },
+                categories,
+                date,
+                date_gmt,
+                excerpt: { rendered: excerptRendered },
+                featured_media_src_url,
+                id,
                 slug,
-                title: titleRendered,
-              };
-              const created = await strapi.services.post.create(postData);
-              resolve(created);
-            } catch (err) {
-              reject(err);
-            }
-          })
-      )
-    );
+                title: { rendered: titleRendered },
+              } = post;
+
+              try {
+                const downloaded = await strapi.config.functions.download(featured_media_src_url);
+                const [{ id: fileId }] = await strapi.config.functions.upload(downloaded);
+
+                const postData = {
+                  author: verifyUserOfPost(author),
+                  content: contentRendered,
+                  categories: verifyCategoryOfPost(categories[0]),
+                  original_date: date,
+                  publish_date: date_gmt,
+                  excerpt: excerptRendered,
+                  featured_image: [fileId],
+                  wp_id: id,
+                  slug,
+                  title: titleRendered,
+                };
+                const created = await strapi.services.post.create(postData);
+                resolve(created);
+              } catch (err) {
+                reject(err);
+              }
+            })
+        )
+      );*/
+    }
 
     ctx.send(posts);
   },
