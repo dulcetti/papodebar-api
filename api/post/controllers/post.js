7c37a914748b7ec8524420deed8f002517dce5e9
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-05-25 23:33:09 -0300

    refactor: rename function and add axios to get infos

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -53,52 +53,62 @@ module.exports = {
     let page = 1;
     const perPage = 1;
 
-    async function test() {
-      const { data } = await axios.get(
-        `https://www.papodebar.com/wp-json/wp/v2/posts?per_page=${perPage}&page=${page}`
-      );
+    async function getPaginatedWpPosts() {
+      const url = `https://www.papodebar.com/wp-json/wp/v2/posts?per_page=${perPage}&page=${page}`;
+      await axios
+        .get(url)
+        .then(async (result) => {
+          const { data } = result;
+          console.info(data);
 
-      /*const posts = await Promise.all(
-        data.map(
-          (post) =>
-            new Promise(async (resolve, reject) => {
-              const {
-                author,
-                content: { rendered: contentRendered },
-                categories,
-                date,
-                date_gmt,
-                excerpt: { rendered: excerptRendered },
-                featured_media_src_url,
-                id,
-                slug,
-                title: { rendered: titleRendered },
-              } = post;
+          /*const posts = await Promise.all(
+            data.map(
+              (post) =>
+                new Promise(async (resolve, reject) => {
+                  const {
+                    author,
+                    content: { rendered: contentRendered },
+                    categories,
+                    date,
+                    date_gmt,
+                    excerpt: { rendered: excerptRendered },
+                    featured_media_src_url,
+                    id,
+                    slug,
+                    title: { rendered: titleRendered },
+                  } = post;
 
-              try {
-                const downloaded = await strapi.config.functions.download(featured_media_src_url);
-                const [{ id: fileId }] = await strapi.config.functions.upload(downloaded);
+                  try {
+                    let fileFeaturedImage = null;
 
-                const postData = {
-                  author: verifyUserOfPost(author),
-                  content: contentRendered,
-                  categories: verifyCategoryOfPost(categories[0]),
-                  original_date: date,
-                  publish_date: date_gmt,
-                  excerpt: excerptRendered,
-                  featured_image: [fileId],
-                  wp_id: id,
-                  slug,
-                  title: titleRendered,
-                };
-                const created = await strapi.services.post.create(postData);
-                resolve(created);
-              } catch (err) {
-                reject(err);
-              }
-            })
-        )
-      );*/
+                    if (featured_media_src_url) {
+                      const downloaded = await strapi.config.functions.download(
+                        featured_media_src_url
+                      );
+                      const [{ id: fileId }] = await strapi.config.functions.upload(downloaded);
+                      fileFeaturedImage = [fileId];
+                    }
+
+                    const postData = {
+                      author: verifyUserOfPost(author),
+                      content: contentRendered,
+                      categories: verifyCategoryOfPost(categories[0]),
+                      original_date: date,
+                      publish_date: date_gmt,
+                      excerpt: excerptRendered,
+                      featured_image: fileFeaturedImage,
+                      wp_id: id,
+                      slug,
+                      title: titleRendered,
+                    };
+                    const created = await strapi.services.post.create(postData);
+                    resolve(created);
+                  } catch (err) {
+                    reject(err);
+                  }
+                })
+            )
+          );*/
     }
 
     ctx.send(posts);
